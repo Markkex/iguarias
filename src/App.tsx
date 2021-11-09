c02570d44e0f7beac7a15975a430e38de7f9b127
Marco Ávila <avila.marco.dev@gmail.com>
2021-11-09 20:55:45 +0000

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
